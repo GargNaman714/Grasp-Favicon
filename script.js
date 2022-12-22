2de265d2f5381810f8e6e841657b10418bf57b63
Naman Garg <garg.naman714@gmail.com>
2022-12-22 22:43:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+
+ isValidURL=string =>{
+    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (res !== null)
+  }
+
+ showIt = () => {
+    var myurl = document.getElementById("urltext").value;
+
+    if (myurl == "")
+        alert("Please Enter URL");
+    else if (isValidURL(myurl) == true)
+    {
+        console.log('correct url');
+        grabFAV();
+    }
+    else
+    {
+        console.log("wrong url")
+        alert("Please Enter Valid URL")
+
+    }
+}
+
+ grabFAV=()=> {
+    var myurl = document.getElementById("urltext").value;
+    var myimg = document.getElementById("theimg");
+    var mysize = document.getElementById("size").value;
+    var duckapi = "https://icons.duckduckgo.com/ip3/"; //duck duckgo api
+    var gapi = "https://s2.googleusercontent.com/s2/favicons?domain="; //google api
+    if (mysize == "default") 
+    {
+        var h1 = "https://";
+        var h2 = "http://";
+
+        myurl = myurl.replace(h1, "");
+        myurl = myurl.replace(h2, "");
+
+        myimg.src = duckapi + myurl + ".ico";
+    }
+    else if (mysize == "16") 
+        myimg.src = gapi + myurl + "&sz=16";
+    else if (mysize == "32") 
+        myimg.src = gapi + myurl + "&sz=32";
+    else if (mysize == "48") 
+        myimg.src = gapi + myurl + "&sz=48";
+    else if (mysize == "64") 
+        myimg.src = gapi + myurl + "&sz=64";
+    else if (mysize == "128") 
+        myimg.src = gapi + myurl + "&sz=128";
+}
